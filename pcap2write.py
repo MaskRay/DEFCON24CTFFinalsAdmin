7b3455325ddc27ee200f3899966c6b82c943ec71
Ray Song <i@maskray.me>
2016-08-07 03:37:38 -0700

    tools

--- pcap2write.py
+++ pcap2write.py
@@ -3,10 +3,9 @@ from itertools import izip
 import argparse, multiprocessing, os, re, struct, subprocess, sys
 
 try:
-    from lxml import etree
     import dpkt
 except ImportError:
-    print('pip2 install --user dpkt lxml')
+    print('pip2 install --user dpkt')
     sys.exit(1)
 if not any(os.access(os.path.join(path, 'tshark'), os.X_OK) for path in os.environ['PATH'].split(os.pathsep)):
     print('please install tshark')
@@ -14,17 +13,20 @@ if not any(os.access(os.path.join(path, 'tshark'), os.X_OK) for path in os.envir
 
 DEVNULL = open(os.devnull, 'r+b')
 # Ref: https://www.wireshark.org/docs/dfref/t/tcp.html
+TCP_IGNORE = '!tcp.analysis.ack_lost_segment and !tcp.analysis.duplicate_ack and !tcp.analysis.retransmission'
+delay_threshold = 0.1
 opt_force = False
 opt_name = ''
 opt_cfe_pov = False
 opt_protocol = ''
+opt_reverse = False
 opt_recursive = False
 opt_substitute = []
 opt_reverse = False
 
 tasks = []
 
-def to_xml_path(pcap_path, i):
+def to_write_path(pcap_path, i):
     base = os.path.splitext(pcap_path)[0]
     if len(opt_substitute):
         base = re.sub(opt_substitute[1], opt_substitute[2], base)
@@ -32,7 +34,18 @@ def to_xml_path(pcap_path, i):
         os.makedirs(base)
     except OSError:
         pass
-    return '{}/{}.xml'.format(base, i)
+    return '{}/{}.write'.format(base, i)
+
+
+def to_read_path(pcap_path, i):
+    base = os.path.splitext(pcap_path)[0]
+    if len(opt_substitute):
+        base = re.sub(opt_substitute[1], opt_substitute[2], base)
+    try:
+        os.makedirs(base)
+    except OSError:
+        pass
+    return '{}/{}.read'.format(base, i)
 
 
 def split_sessions(pcap_path):
@@ -65,7 +78,6 @@ def split_sessions(pcap_path):
                 ip = ether.data
                 if not hasattr(ip, 'tcp'): continue
                 tcp = ip.tcp
-                # assume the first data packet is sent by the client
                 client_ip = struct.unpack('>I', ip.src)[0]
                 server_ip = struct.unpack('>I', ip.dst)[0]
                 if stream_id not in sessions:
@@ -102,22 +114,10 @@ def split_sessions(pcap_path):
         if opt_protocol == 'tcp' and not ('has_begin' in session and 'has_end' in session):
             continue
 
-        # Header
-        # Ref: https://github.com/CyberGrandChallenge/cgc-release-documentation/blob/master/pov-markup-spec.txt
-        if opt_cfe_pov:
-            root = etree.Element('cfepov')
-        else:
-            root = etree.Element('pov')
-        cbid = etree.Element('cbid')
-        cbid.text = opt_name
-        root.append(cbid)
-        replay = etree.Element('replay')
-        if opt_cfe_pov:
-            replay.append(etree.fromstring('<negotiate><type2 /></negotiate>'))
-
         # I/O
-        first = True
-        last = 0
+        payload = ''
+        payload2 = ''
+        t = session['client_ip']
         for ts, pkt in session['packets']:
             if opt_protocol == 'cgc':
                 src_ip = struct.unpack('B', pkt[14])[0] # side, 0: from client
@@ -131,66 +131,42 @@ def split_sessions(pcap_path):
                 ip = pkt
                 data = ip.udp.data
             if len(data) > 0:
-                if ts-last > delay_threshold and not first:
-                    delay = etree.Element('delay')
-                    delay.text = str(int(round((ts-last)*1000)))
-                    replay.append(delay)
-                payload = ''
-                if (src_ip == session['client_ip']) == opt_reverse:
-                    node = etree.Element('read')
-                    length = etree.Element('length')
-                    length.text = str(len(data))
-                    node.append(length)
+                if (src_ip == t) == opt_reverse:
+                    payload += data
                 else:
-                    node = etree.Element('write')
-                    node_data = etree.Element('data')
-                    for c in data:
-                        if 33 <= ord(c) < 127 and c not in '&<>\\':
-                            payload += c
-                        else:
-                            payload += '\\x%0*x' % (2, ord(c))
-                    node_data.text = payload
-                    node.append(node_data)
-                replay.append(node)
-                first = False
-                last = ts
+                    payload2 += data
 
         # Write
-        root.append(replay)
-        xml_path = to_xml_path(pcap_path, i)
+        write_path = to_write_path(pcap_path, i)
         i += 1
-        with open(xml_path, 'w') as f:
-            f.write('<?xml version="1.0" standalone="no" ?>\n')
-            if opt_cfe_pov:
-                f.write('<!DOCTYPE cfepov SYSTEM "/usr/share/cgc-docs/cfe-pov.dtd">\n')
-            else:
-                f.write('<!DOCTYPE pov SYSTEM "/usr/share/cgc-docs/replay.dtd">\n')
-            s = etree.tostring(root, pretty_print=True)
-            s = re.sub('<(read|write)>\n      ', '<\\1>', s)
-            s = re.sub('\n    </(read|write)>', '</\\1>', s)
-            f.write(s)
+        with open(write_path, 'wb') as f:
+            f.write(payload)
+        read_path = to_read_path(pcap_path, i)
+        i += 1
+        with open(read_path, 'wb') as f:
+            f.write(payload2)
 
-    print('Found {} session(s){}'.format(i, ', generated {}'.format(to_xml_path(pcap_path, '*')) if i > 0 else ''))
+    print('Found {} session(s){}'.format(i, ', generated {}'.format(to_write_path(pcap_path, '*')) if i > 0 else ''))
 
 
 def main():
-    global delay_threshold, opt_cfe_pov, opt_force, opt_name, opt_protocol, opt_recursive, opt_substitute, opt_reverse
-    ap = argparse.ArgumentParser(description='pcap2xml',
+    global delay_threshold, opt_cfe_pov, opt_force, opt_name, opt_protocol, opt_recursive, opt_substitute,opt_reverse
+    ap = argparse.ArgumentParser(description='pcap2write',
                                  formatter_class=argparse.RawDescriptionHelpFormatter, epilog='''
 Ethernet-IP-TCP packets (captured by tcpdump, dumpcap...) and ethertype 0xffff packets (by cb-packet-log) are both supported.
 
 Examples:
-./pcap2xml.py /tmp/a.pcap /tmp/b.cap # /tmp/a.pcap -> /tmp/a.xml ; /tmp/b.cap -> /tmp/b.xml
-./pcap2xml.py /tmp/dir/ # /tmp/dir/*.(cap|pcap) -> /tmp/dir/*.$session_id.xml
-./pcap2xml.py -c /tmp/dir/ # <!DOCTYPE cfe-pov SYSTEM "/usr/share/cgc-docs/cfe-pov.dtd">
-./pcap2xml.py -d 0.1 /tmp/dir/ # time intervals between two I/O operations greater than 0.1s are treated as <delay> elements
-./pcap2xml.py -n name /tmp/dir # <cbid>$name</cbid>
-./pcap2xml.py -p 4 /tmp/dir # 4 parallel workers
-./pcap2xml.py -r /tmp/dir/ # /tmp/dir/**/*.(cap|pcap) -> /tmp/dir/**/*.$session_id.xml
-./pcap2xml.py -s /root/roots /tmp/root # /tmp/root/*.(cap|pcap) -> /tmp/roots/*.$session_id.xml
+./pcap2write.py /tmp/a.pcap /tmp/b.cap # /tmp/a.pcap -> /tmp/a.write ; /tmp/b.cap -> /tmp/b.write
+./pcap2write.py /tmp/dir/ # /tmp/dir/*.(cap|pcap) -> /tmp/dir/*.$session_id.write
+./pcap2write.py -c /tmp/dir/ # <!DOCTYPE cfe-pov SYSTEM "/usr/share/cgc-docs/cfe-pov.dtd">
+./pcap2write.py -d 0.1 /tmp/dir/ # time intervals between two I/O operations greater than 0.1s are treated as <delay> elements
+./pcap2write.py -n name /tmp/dir # <cbid>$name</cbid>
+./pcap2write.py -p 4 /tmp/dir # 4 parallel workers
+./pcap2write.py -r /tmp/dir/ # /tmp/dir/**/*.(cap|pcap) -> /tmp/dir/**/*.$session_id.write
+./pcap2write.py -s /root/roots /tmp/root # /tmp/root/*.(cap|pcap) -> /tmp/roots/*.$session_id.write
 ''')
     ap.add_argument('-c', '--cfe-pov', action='store_true', help='generate XML files of type cfe-pov.dtd (<cfepov>)')
-    ap.add_argument('-d', '--delay-threshold', type=float, default=0.1, help='insert a <delay> element if the time interval between two I/O is larger than the specified value')
+    ap.add_argument('-d', '--delay-threshold', type=float, help='insert a <delay> element if the time interval between two I/O is larger than the specified value')
     ap.add_argument('-f', '--force', action='store_true', help='override existent XML files')
     ap.add_argument('-P', '--protocol', default='udp', help='cgc/udp/tcp (default: udp)')
     ap.add_argument('-p', '--parallel', type=int, default=0, help='number of parallel workers (default: 0, the number of cores)')
@@ -206,7 +182,6 @@ Examples:
     opt_cfe_pov = args.cfe_pov
     opt_name = args.name
     opt_protocol = args.protocol
-    opt_reverse = args.reverse
     if opt_protocol not in ['cgc', 'tcp', 'udp']:
         ap.print_help()
     if args.substitute:
@@ -223,7 +198,7 @@ Examples:
                     walk(os.path.join(path, i), depth+1)
         elif re.search(r'\.(cap|pcap)$', path):
             try:
-                if opt_force or os.path.getsize(to_xml_path(path, 0)) == 0:
+                if opt_force or os.path.getsize(to_write_path(path, 0)) == 0:
                     tasks.append(path)
             except OSError:
                 tasks.append(path)
